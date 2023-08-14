db67fef3b7135e2a523a3ad6f655427cd5433d84
yorranan <yorranan.almeida@gmail.com>
2023-08-14 09:03:52 -0300

    inserted bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsComponent } from './components/components.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { AuthComponent } from './components/auth/auth.component';
 import { DashboardComponent } from './components/auth/dashboard/dashboard.component';
 import { SettingsComponent } from './components/auth/settings/settings.component';
-import { GratificationComponent } from './components/gratification/gratification.component';
-import { TaskComponent } from './components/task/task.component';
-import { UserComponent } from './components/user/user.component';
+import { GratificationComponent } from './components/auth/gratification/gratification.component';
+import { TaskComponent } from './components/auth/task/task.component';
+import { UserComponent } from './components/auth/user/user.component';
 import { SessionComponent } from './components/session/session.component';
+import { AuthComponent } from './components/auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent,
     SignInComponent,
     SignUpComponent,
-    AuthComponent,
     DashboardComponent,
     SettingsComponent,
     GratificationComponent,
     TaskComponent,
     UserComponent,
-    SessionComponent
+    SessionComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
