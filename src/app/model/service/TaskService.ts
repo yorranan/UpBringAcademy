2d62897adc7cbb45cd93f16dc94e3f6f1b452819
LorenzoPrograming <lm.pilati@gmail.com>
2023-08-31 13:30:05 -0300

    conversor data e hora2

--- src/app/model/service/TaskService.ts
+++ src/app/model/service/TaskService.ts
@@ -9,7 +9,14 @@ export default class TaskService{
         "Levar o lixo da cozinha para fora",
         100,
         new Date('2023-09-01T15:30'),
-        new Date('2023-09-01T15:30')
+        new Date('2023-09-01T17:30')
+        ));
+        this._tasks.push(new Task(
+            "Andar com o cahcorro",
+            "Andar com o Maxiel",
+            50,
+            new Date('2023-09-02T14:30'),
+            new Date('2023-09-02T17:30')
         ));
     }
 
--- src/app/model/util/DateTimeConverter.ts
+++ src/app/model/util/DateTimeConverter.ts
@@ -1,5 +1,19 @@
 export default class DateTimeConverter{
-    convert(data: Date): string{
-        return `${data.getDay()}/${data.getMonth()}/${data.getFullYear()} ${data.getHours()}:${data.getMinutes()}`;
+    convert(date: Date): string{
+        const today = new Date();
+        const tomorrow = new Date()
+        const yesterday = new Date()
+
+        tomorrow.setDate(tomorrow.getDate()+1);
+        yesterday.setDate(yesterday.getDate()-1);
+
+        if(date.toDateString() === today.toDateString())
+            return `Hoje ${date.getHours()}:${date.getMinutes()}`;
+        if(date.toDateString() === tomorrow.toDateString())
+            return `Amanhã ${date.getHours()}:${date.getMinutes()}`;
+        if(date.toDateString() === yesterday.toDateString())
+            return `Ontem ${date.getHours()}:${date.getMinutes()}`;
+
+        return `${date.getDate()}/${(date.getMonth()+1)}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
     }
 }
