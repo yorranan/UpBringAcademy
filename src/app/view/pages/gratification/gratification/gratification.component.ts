5d48700c81dc82e5c493ab9ccefc57b32397f9d7
yorranan <yorranan.costa@outlook.com>
2023-09-04 15:52:32 -0300

    variaveis adicionadas

--- src/app/view/pages/gratification/gratification/gratification.component.ts
+++ src/app/view/pages/gratification/gratification/gratification.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import Gratification from "../../../../model/entities/Gratification";
 @Component({
   selector: 'app-gratification',
   templateUrl: './gratification.component.html',
   styleUrls: ['./gratification.component.css']
 })
 export class GratificationComponent {
-
+  name: string = "Férias, Eu quero Férias";
+  description: string = "Lorem ipsum dolor sit amet,  " +
+    "incididunt ut labore et dolore magna aliqua.";
+  points: number = 100;
 }
--- src/app/view/pages/task/task/task.component.ts
+++ src/app/view/pages/task/task/task.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent {
-
+  name: string = "Tarefa 1"
+  description: string = "Lorem ipsum dolor sit amet";
+  points: number = 100;
+  beginDateTime: Date = new Date()
+  endDateTime: Date = new Date();
 }
