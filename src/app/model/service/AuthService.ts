7cc9716bffc46489cd661f7c2e24940977f6e1ea
LorenzoPrograming <lm.pilati@gmail.com>
2023-09-09 17:07:17 -0300

    sessão concertada

--- src/app/model/service/AuthService.ts
+++ src/app/model/service/AuthService.ts
@@ -1,4 +1,4 @@
-import { Injectable, NgZone } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/compat/auth";
 import { AngularFirestore } from "@angular/fire/compat/firestore";
 import { Router } from "@angular/router";
@@ -8,34 +8,31 @@ import { Router } from "@angular/router";
 })
 export default class AuthService{
     userData: any;
+    isLoggedIn: boolean;
 
-    constructor(private router: Router, private firebaseAuth: AngularFireAuth, private firastore: AngularFirestore, private ngZone: NgZone){
+    constructor(private router: Router, private firebaseAuth: AngularFireAuth, private firastore: AngularFirestore){
+        this.updateUser();
+        this.isLoggedIn = !!sessionStorage.getItem('user');
+    }
+
+    updateUser(){
         this.firebaseAuth.authState.subscribe(user => {
             if(user){
                 this.userData = user;
-                localStorage.setItem('user', JSON.stringify(this.userData));
-                JSON.parse(localStorage.getItem('user')!);
+                sessionStorage.setItem('user', JSON.stringify(this.userData));
+                JSON.parse(sessionStorage.getItem('user')!);
+                this.isLoggedIn = true;
             }else{
-                localStorage.setItem('user', null);
-                JSON.parse(localStorage.getItem('user')!);
+                sessionStorage.setItem('user', null);
+                JSON.parse(sessionStorage.getItem('user')!);
             }
         });
     }
 
-    get isLoggedIn(): boolean{
-        const user = JSON.parse(localStorage.getItem('user')!);
-        if(user == null){
-            return false;
-        }else{
-            return true;
-        }
-    }
-
     logIn(email: string, password: string){
-        return this.firebaseAuth.signInWithEmailAndPassword(email, password).then((result: any) =>{
-            this.ngZone.run(() => {
-                this.router.navigate(['dashboard']);
-            })
+        return this.firebaseAuth.signInWithEmailAndPassword(email, password).then(result =>{
+            this.updateUser();
+            this.router.navigate(['dashboard']);
         }).catch((error: any) =>{
             window.alert(error);
         });
@@ -43,7 +40,7 @@ export default class AuthService{
 
     logOut(){
         return this.firebaseAuth.signOut().then(() => {
-            localStorage.removeItem('user');
+            sessionStorage.removeItem('user');
             this.router.navigate(['login']);
         });
     }
