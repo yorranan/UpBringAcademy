12cc9000c592d7ad8e48b4f9c0bb695c3f0e2e1f
Luiz Carlos <lsouzapedroso@gmail.com>
2023-09-13 00:32:07 -0300

    Update GratificationService.ts

--- src/app/model/service/GratificationService.ts
+++ src/app/model/service/GratificationService.ts
@@ -4,44 +4,44 @@ import ICRUDService from "./ICRUDService";
 import { Injectable } from "@angular/core";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export default class GratificationService implements ICRUDService<Gratification>{
-    private PATH: string = 'gratifications'
+  private PATH: string = 'gratifications'
 
-    constructor(private firestore: AngularFirestore){}
+  constructor(private firestore: AngularFirestore){}
 
-    create(gratification: Gratification){
-        gratification.redeemDateTime = [];
-        return this.firestore.collection(this.PATH).add(mapper(gratification));
-    }
+  create(gratification: Gratification){
+    gratification.redeemDateTime = [];
+    return this.firestore.collection(this.PATH).add(mapper(gratification));
+  }
 
-    read(id: string){
-        return this.firestore.collection(this.PATH).doc(id).snapshotChanges();
-    }
+  read(id: string){
+    return this.firestore.collection(this.PATH).doc(id).snapshotChanges();
+  }
 
-    update(id: string, gratification: Gratification){
-        return this.firestore.collection(this.PATH).doc(id).update(mapper(gratification))
-    }
+  update(id: string, gratification: Gratification){
+    return this.firestore.collection(this.PATH).doc(id).update(mapper(gratification))
+  }
 
-    delete(id: string){
-        return this.firestore.collection(this.PATH).doc(id).delete()
-    }
+  delete(id: string){
+    return this.firestore.collection(this.PATH).doc(id).delete()
+  }
 
-    getByParent(parentId: string){
-        return this.firestore.collection(this.PATH, ref => ref.where('parentId', '==', parentId)).snapshotChanges()
-    }
+  getByParent(parentId: string){
+    return this.firestore.collection(this.PATH, ref => ref.where('parentId', '==', parentId)).snapshotChanges()
+  }
 }
 
 function mapper(gratification: Gratification){
-    return {
-        name: gratification.name,
-        description: gratification.description,
-        points: gratification.points,
-        quantity: gratification.quantity,
-        parentId: gratification.parentId,
-        redeemDateTime: gratification.redeemDateTime.map(redeem => (
-            {childId: redeem.childId, dateTime: redeem.dateTime}
-        ))
-    };
+  return {
+    name: gratification.name,
+    description: gratification.description,
+    points: gratification.points,
+    quantity: gratification.quantity,
+    parentId: gratification.parentId,
+    redeemDateTime: gratification.redeemDateTime.map(redeem => (
+      {childId: redeem.childId, dateTime: redeem.dateTime}
+    ))
+  };
 }
